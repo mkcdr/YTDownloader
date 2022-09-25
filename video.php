56fd73c74a708858862ccd4a6de9b6128cce4a7c
mkcdr <113985520+mkcdr@users.noreply.github.com>
2022-09-25 23:31:31 +0300

    Update video.php

--- video.php
+++ video.php
@@ -1,8 +1,8 @@
 <?php
 
-header('Content-Type: application/json');
-
 require_once __DIR__ . '/YTDownloader.php';
+
+header('Content-Type: application/json');
 
 $url = isset($_GET['url']) ? $_GET['url'] : null;
 
